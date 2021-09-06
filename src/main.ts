7c09a144aca25978645f930c046158183ce6a508
kyanagimoto <koichiyanagimoto@gmail.com>
2021-09-06 22:00:28 +0900

    fix: 🐛 change jq lib

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import * as semver from 'semver'
 import axios from 'axios'
-import * as jq from 'node-jq'
+import jq from 'jqts';
 import {Convert, Response} from './response'
 //import {wait} from './wait'
 
@@ -15,17 +15,13 @@ async function run(): Promise<void> {
       repoName +
       '/tags?page_size=100'
     const res = await axios.get<Response[]>(url)
-    const response = Convert.toResponse(JSON.stringify(res.data))
+    const response = JSON.parse(JSON.stringify(res.data))
 
-    const filter = '.results'
-    const result = await jq.run(filter, response, {input: 'json', output: 'json'})
-    const filter2 = 'map(.name)'
-    const result2 = await jq.run(filter2, result, {input: 'json', output: 'json'})
-    console.log(result2)
-    const versions = JSON.parse(JSON.stringify(result2))
-
-    const latest = semver.maxSatisfying(versions, '*')
-    console.log(latest)
+    const pattern = jq.compile('.results | map(.name) | .[]');
+    const versions = pattern.evaluate(response)
+    console.log(versions)
+    const latest = semver.maxSatisfying(JSON.parse(JSON.stringify(versions)), '*')
+    
     core.setOutput('latest', latest)
     core.exportVariable('latest', latest)
   } catch (error) {
