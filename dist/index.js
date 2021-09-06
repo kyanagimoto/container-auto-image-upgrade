ace976d2eb56baef915ac1ad45e65cf674d5aa7b
kyanagimoto <koichiyanagimoto@gmail.com>
2021-09-06 17:00:24 +0900

    fix: 🐛 change build

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 3109:
@@ -47,8 +47,8 @@ const response_1 = __nccwpck_require__(3280);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            //const repoName = core.getInput("repo-name", { required: true })
-            const repoName = "elastic/apm-server";
+            const repoName = core.getInput("repo-name", { required: true });
+            //const repoName = "elastic/apm-server"
             let url = "https://hub.docker.com/v2/repositories/" + repoName + "/tags?page_size=100";
             const res = yield axios_1.default.get(url);
             const response = response_1.Convert.toResponse(JSON.stringify(res.data));
@@ -21603,4 +21603,3 @@ module.exports = require("zlib");
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
